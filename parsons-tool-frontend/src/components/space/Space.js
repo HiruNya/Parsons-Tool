9e680ed3c2c367342f989a9ea6c21b6ce162e7c5
Hiruna Jayamanne <hiru@hiru.dev>
2022-06-29 03:00:59 +1200

    Refactor to use standardised problem format

--- parsons-tool-frontend/src/components/space/Space.js
+++ parsons-tool-frontend/src/components/space/Space.js
@@ -1,5 +1,4 @@
-import { useCallback, useState } from 'react'
-import update from 'immutability-helper'
+import { useCallback} from 'react'
 
 import Block from '../blocks/Block'
 import "./space.css"
@@ -8,74 +7,7 @@ const style = {
     width: 400,
 }
 
-const INITIAL_CODE = 'print($$fade$$)\n' +
-    '  print("Par$$fade$$")\n' +
-    'print("$$fade$$blems")'
-
-const FADE_TOKEN = "$$fade$$";
-const mapLine = (text, id) => {
-    let parsedText = text;
-    const fadedIndices = []
-    while (parsedText.indexOf(FADE_TOKEN) >= 0) {
-        const i = parsedText.indexOf(FADE_TOKEN);
-        fadedIndices.push(i)
-        parsedText = parsedText.replace(FADE_TOKEN, "");
-    }
-    const indentation = Math.floor(countSpaces(parsedText) / 2);
-    return {id, text: parsedText, fadedIndices, indentation};
-}
-const countSpaces = (line) => {
-    let i = 0;
-    let count = 0;
-    walkLine: while (i < line.length) {
-        const c = line.charAt(i)
-        switch (c) {
-            case " ":
-                count++;
-                break;
-            case "\t":
-                count += 2;
-                break;
-            default:
-                break walkLine;
-        }
-        i++;
-    }
-    return count;
-}
-
-const Space = () => {
-
-    const [cards, setCards] = useState(INITIAL_CODE.split("\n").map(mapLine))
-    const moveCard = useCallback((dragIndex, hoverIndex, indentationDiff) => {
-        if (hoverIndex) {
-            console.log("Moved", cards[dragIndex].id, "to", cards[hoverIndex].id, "+", indentationDiff);
-            setCards((prevCards) => {
-                const movedCard = prevCards[dragIndex];
-                movedCard.indentation = Math.max(0, movedCard.indentation + indentationDiff);
-                return update(prevCards, {
-                    $splice: [
-                        [dragIndex, 1],
-                        [hoverIndex, 0, movedCard],
-                    ],
-                });
-            })
-        } else {
-            setCards((prevCards) => {
-                const c = update(prevCards, {
-                    [dragIndex]: {
-                        indentation: {
-                            // $apply: (prevIndent) => Math.min(Math.max(0, prevIndent + indentationDiff), 10)
-                            $set: Math.min(Math.max(0, indentationDiff), 10)
-                        }
-                    }
-                });
-                return c;
-            })
-        }
-    }, [])
-    console.log(cards.map(({id}) => id))
-    const matches = (cards.find(({id}, i) => id !== i) === undefined)? "matches": "not-matches";
+const Space = ({ blocks, moveCard, matches }) => {
     const renderCard = useCallback((card, index) => {
         return (
             <Block
@@ -88,10 +20,10 @@ const Space = () => {
                 indentation={card.indentation}
             />
         )
-    }, [])
+    }, [moveCard])
     return (
         <>
-            <div className={"parsons-space " + matches} style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
+            <div className={"parsons-space " + matches} style={style}>{blocks.map(renderCard)}</div>
         </>
     )
 }
--- parsons-tool-frontend/src/pages/problemEvaluation.js
+++ parsons-tool-frontend/src/pages/problemEvaluation.js
@@ -1,16 +1,46 @@
 import Space from "../components/space/Space";
-import React from "react";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
+import React, {useCallback, useState} from "react";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {DndProvider} from "react-dnd";
+import update from "immutability-helper";
+import data from "../data/recursion-parsons.json";
 
 function ProblemEvaluation() {
-  return (
-    <div className="App">
-      <DndProvider backend={HTML5Backend}>
-        <Space />
-      </DndProvider>
-    </div>
-  );
+    const [cards, setCards] = useState(() => data.problems[1].problem.blocks)
+    const moveCard = useCallback((dragIndex, hoverIndex, indentationDiff) => {
+        if (hoverIndex) {
+            console.log("Moved", cards[dragIndex].id, "to", cards[hoverIndex].id, "+", indentationDiff);
+            setCards((prevCards) => {
+                const movedCard = prevCards[dragIndex];
+                movedCard.indentation = Math.max(0, movedCard.indentation + indentationDiff);
+                return update(prevCards, {
+                    $splice: [
+                        [dragIndex, 1],
+                        [hoverIndex, 0, movedCard],
+                    ],
+                });
+            })
+        } else {
+            setCards((prevCards) =>
+                update(prevCards, {
+                    [dragIndex]: {
+                        indentation: {
+                            // $apply: (prevIndent) => Math.min(Math.max(0, prevIndent + indentationDiff), 10)
+                            $set: Math.min(Math.max(0, indentationDiff), 10)
+                        }
+                    }
+                }))
+        }
+    }, [cards])
+    const matches = (cards.find(({id}, i) => id !== data.problems[0].problem.solution[i]) === undefined) ? "matches" : "not-matches";
+
+    return (
+        <div className="App">
+            <DndProvider backend={HTML5Backend}>
+                <Space blocks={cards} moveCard={moveCard} matches={matches}/>
+            </DndProvider>
+        </div>
+    );
 }
 
 export default ProblemEvaluation;
