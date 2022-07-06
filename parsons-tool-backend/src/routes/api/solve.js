5e366c1d700cad32d7bbd213ce5a3e9d3b9d5b46
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-07 01:01:54 +1200

    Setup basic routes

---
+++ parsons-tool-backend/src/routes/api/solve.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/solve', async (req, res) => {
+    res.json("Solve endpoint listening");
+})
+
+export default router;
--- parsons-tool-backend/src/routes/index.js
+++ parsons-tool-backend/src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import api from './api/api';
+import {api, solve, problems} from './api/*';
 
 const router = express.Router();
 
 
 router.use('/api', api);
-
+router.use('/solve', solve);
+router.use('/problems', problems)
 
 export default router;
