ddc7305f57594e4fc3f679ce6e3fd3eb034283a9
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-12 13:19:43 +1200

    removed border that shows if its correct or wrong

--- parsons-tool-frontend/src/components/space/Space.js
+++ parsons-tool-frontend/src/components/space/Space.js
@@ -21,12 +21,7 @@ const Space = ({ name, blocks, matches }) => {
     );
   }, []);
   return (
-    <div
-      className={`p-2 ml-2 mr-2 w-1/2 h-96 border-solid rounded-lg border-2 ${
-        matches ? 'border-green-600' : 'border-red-600'
-      }`}
-      ref={setNodeRef}
-    >
+    <div className="p-2 ml-2 mr-2 w-1/2 h-96 border-solid rounded-lg border-2 border-gray-400" ref={setNodeRef}>
       <SortableContext items={blocks} id={name}>
         {blocks.map(renderCard)}
       </SortableContext>
