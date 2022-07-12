a177a7f3ce32be0f315ffe2258e016ac805709ac
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-12 20:54:46 +1200

    Create student endpoint + route

--- parsons-tool-backend/src/routes/api/student.js
+++ parsons-tool-backend/src/routes/api/student.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import ParsonsProblems from '../../database/ProblemSchema';
 
 const router = express.Router();
 
-router.get('/student', async (req, res) => {
-  res.json('Student endpoint hit');
+router.get('/', async (req, res) => {
+  try {
+    const problems = await ParsonsProblems.find({});
+    res.json(problems);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('An issue has occured on the server end');
+  }
 });
 
 export default router;
--- parsons-tool-backend/src/routes/index.js
+++ parsons-tool-backend/src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import api from './api/api';
 import solve from './api/solve';
 import problems from './api/problems';
+import student from './api/student';
 
 const router = express.Router();
 
 router.use('/api', api);
 router.use('/solve', solve);
 router.use('/problems', problems);
+router.use('/student', student);
 
 export default router;
