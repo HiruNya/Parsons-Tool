14b8e7673f8755b2f3be1aed991d32f6a0ebab4a
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-18 13:00:57 +1200

    created custom get/post hooks

---
+++ parsons-tool-frontend/src/components/hooks/useGet.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+/**
+ * A custom hook which fetches data from the given URL. Includes functionality to determine whether
+ * data is being loaded still or if an error has occurred
+ */
+export default function useGet(url, initialState = null) {
+  const [data, setData] = useState(initialState);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      setError(false);
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+        setLoading(false);
+      } catch {
+        setError(true);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+}
