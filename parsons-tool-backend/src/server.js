c447f13cbe74862e1210d2af61976b7c1aa4495a
Hiruna Jayamanne <hiru@hiru.dev>
2022-09-18 06:41:58 +1200

    Replace esm with nodejs modules

--- parsons-tool-backend/src/server.js
+++ parsons-tool-backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
-import path from 'path';
-import routes from './routes';
+import router from './routes';
 
 // Setup Express
 const app = express();
@@ -14,7 +14,6 @@ console.log('Using MongoDB URL', mongoDbUrl);
 app.use(express.json());
 
 // Setup CORS for frontend access to server
-const cors = require('cors');
 const corsOptions = {
   origin: '*',
   credentials: true,
@@ -23,23 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Setup API Routes.
-app.use('/', routes);
-
-// Make the public folder available statically
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve up the frontend's "build" directory, if we're running in production mode.
-if (process.env.NODE_ENV === 'production') {
-  console.log('Running in production');
-
-  // Make all files in that folder public
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  // If we get any GET requeset we can't proces using one of the server routes, serve up index.html by default.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-  });
-}
+app.use('/', router);
 
 // Connect to the database and then start the server
 mongoose
