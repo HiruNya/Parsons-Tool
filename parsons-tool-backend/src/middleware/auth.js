fe09743e006e8bd0dcb968b8f8e1677e7120f5e2
Hiruna Jayamanne <hiru@hiru.dev>
2022-09-18 06:43:31 +1200

    Add firebase auth middleware

---
+++ parsons-tool-backend/src/middleware/auth.js
@@ -0,0 +1,26 @@
+import admin from 'firebase-admin';
+import firebaseConfig from './firebaseConfig.json' assert { type: 'json' };
+
+console.log('FirebaseConfig', firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+});
+
+export const firebaseAuth =
+  (requireAuth = false) =>
+  async (req, res, next) => {
+    // https://fireship.io/snippets/express-middleware-auth-token-firebase/
+    console.log(req);
+    if (req.headers && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      const idToken = req.headers.authorization.split('Bearer ')[1];
+
+      try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        req.currentUser = decodedToken;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    next();
+  };
