ed9ba2a5020d87f70925143110c6d214f3626c0e
Hiruna Jayamanne <hiru@hiru.dev>
2022-10-01 15:15:32 +1300

    Add hover and focus effects onto back button

--- parsons-tool-frontend/src/components/BackButton.js
+++ parsons-tool-frontend/src/components/BackButton.js
@@ -4,7 +4,10 @@ export default function BackButton({ text }) {
   const navigate = useNavigate();
 
   return (
-    <button className="bg-yellow-200 p-2 rounded-lg absolute left-5" onClick={() => navigate(-1)}>
+    <button
+      className="bg-yellow-200 hover:bg-yellow-300 focus:bg-yellow-300 p-2 rounded-lg absolute left-5"
+      onClick={() => navigate(-1)}
+    >
       {text || '←'}
     </button>
   );
